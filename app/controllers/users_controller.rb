aa1f69ec8df56dd85642151a8037cd2617e3a8ea
tmt-mateusz <tmt.ocb@gmail.com>
2020-11-25 05:03:41 +0100

    Edit template for users and update profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,22 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def edit
+		@user = User.find(params[:id])  #This will find-out an user based on the id in user table
+	end
+
+	def update
+		@user = User.find(params[:id])
+			if @user.update(user_params)
+			flash[:notice] = "Your account info was updated"
+			redirect_to articles_path
+		else
+			render 'edit'
+		end
+	end	
+
+
+
 	def create
 		@user = User.new(user_params)
 		if @user.save
