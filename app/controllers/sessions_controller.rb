0fca74aea354304527953735cb07de299e0c23f7
tmt-mateusz <tmt.ocb@gmail.com>
2020-11-25 21:07:17 +0100

    Sessions: Log in and Logout for users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,21 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			session[:user_id] = user.id #Allows login users to authenticate, encrypted Cookie data
+			flash[:notice] = "Logged in successfully"
+			redirect_to user
+		else
+			flash.now[:alert] = "There was something wrong with login details"
+			render 'new'	
+		end
 	end
 
 	def destroy
+		session[:user_id] = nil #logout users
+		flash[:notice] = "Logged out"
+		redirect_to root_path
 	end
 		
 	end
